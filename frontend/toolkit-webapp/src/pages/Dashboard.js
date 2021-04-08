652cfcd5411c4cd00986ec253659be21b997c906
Nathaniel Lyra <nlyra@knights.ucf.edu>
2021-04-08 15:27:22 -0400

    Added preliminary toolbar

--- frontend/toolkit-webapp/src/pages/Dashboard.js
+++ frontend/toolkit-webapp/src/pages/Dashboard.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
-import {Button, Card, CardActions, Container, CssBaseline, Divider, makeStyles, Grid, CardMedia, CardContent, Typography, Paper} from '@material-ui/core'
+import {AppBar, Toolbar, Button, Card, CardActions, Container, CssBaseline, 
+        Divider, makeStyles, Grid, CardMedia, CardContent, Typography, Paper,
+        IconButton, Menu} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
 import '../css/dashboard.css'
 
-const dashStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({
 
     container:
     {
@@ -25,83 +29,97 @@ const dashStyles = makeStyles((theme) => ({
     CardContent:
     {
         flexGrow: 1,
+    },
+    grow:
+    {
+        flexGrow: 1
+    },
+    toolBarColor:
+    {
+        background: "black"
     }
 }))
 
 const cards = [1, 2, 3, 4, 5, 6]
 const Dashboard = () => {
-    
-    const classes = dashStyles()
-    
+
+    const classes = useStyles()
     return (
         <div>
-        <CssBaseline />
-          <Container maxWidth = "lg" className = {classes.container}>
-              <div className = 'createModules'>
-                  <Grid container spacing={3} justify="center">
-                    <Grid item>
-                        <Button variant="contained">
-                            Create Course
+            <div className={classes.grow}>
+                <AppBar position="static" className={classes.toolBarColor}>
+                    <Toolbar>
+                        <IconButton>
+                            <MenuIcon style={{color: "white"}}></MenuIcon>
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+            </div>
+            <CssBaseline />
+            <Container maxWidth="lg" className={classes.container}>
+                <div className='createModules'>
+                    <Grid container spacing={3} justify="center">
+                        <Grid item>
+                            <Button variant="contained">
+                                Create Course
+                            </Button>
+                        </Grid>
+
+                        <Grid item>
+                            <Button variant="contained">
+                                My Courses
+                            </Button>
+                        </Grid>
+
+                        <Grid item>
+                            <Button variant="contained">
+                                My Files
+                            </Button>
+                        </Grid>
+
+                        <Grid item>
+                            <Button variant="contained">
+                                Calendar
                         </Button>
+                        </Grid>
+
                     </Grid>
+                </div>
 
-                    <Grid item>
-                        <Button variant="contained">
-                            My Courses
-                        </Button>
+                <div className='modules'>
+                    <Grid container spacing={3}>
+                        {cards.map((card) => (
+
+                            <Grid item key={card} xs={12} sm={4} md={3}>
+                                <Card className={classes.card}>
+                                    <CardMedia
+                                        className={classes.cardMedia}
+                                        image="https://source.unsplash.com/random"
+                                        title="Title"
+                                    />
+                                    <CardContent className={classes.CardContent}>
+                                        <Typography variant="h5">
+                                            Module Name
+                                        </Typography>
+                                        <Typography gutterBottom>
+                                            Placeholder for the module content.
+                                        </Typography>
+                                        <CardActions>
+                                            <Button variant="outlined" size="small">
+                                                View Module
+                                            </Button>
+                                            <Button variant="outlined" size="small">
+                                                Edit
+                                            </Button>
+                                        </CardActions>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+
+                        ))}
                     </Grid>
-
-                    <Grid item>
-                        <Button variant="contained">
-                            My Files
-                        </Button>
-                    </Grid>
-
-                    <Grid item>
-                        <Button variant="contained">
-                            Calendar
-                        </Button>
-                    </Grid>
-
-                  </Grid>
-              </div>
-
-              <div className = 'modules'>
-              <Grid container spacing={3}>
-                  {cards.map((card) => (
-
-                    <Grid item key={card} xs={12} sm={4} md={3}>
-                    <Card className = {classes.card}>
-                        <CardMedia
-                        className = {classes.cardMedia}
-                        image= "https://source.unsplash.com/random"
-                        title= "Title"
-                        />
-                        <CardContent className = {classes.CardContent}>
-                            <Typography variant="h5">
-                                Module Name
-                            </Typography>
-                            <Typography gutterBottom>
-                                Placeholder for the module content.
-                            </Typography>
-                            <CardActions>
-                                <Button variant="outlined" size = "small">
-                                View Module
-                                </Button>
-                                <Button variant="outlined" size = "small">
-                                Edit
-                                </Button>
-                            </CardActions>
-                        </CardContent>
-                    </Card>
-                    </Grid>
-
-                  ))}
-
-
-                </Grid>
-              </div>
-          </Container>
+                </div>
+            </Container>
         </div>
     )
 }
