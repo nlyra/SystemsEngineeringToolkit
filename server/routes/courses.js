6b3b36b8544b5f5d965f979ad56fef9007cd93aa
Michael-Dews <79720787+Michael-Dews@users.noreply.github.com>
2021-06-20 17:35:06 -0400

    this should be working now

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -4,6 +4,9 @@ const VerifyToken = require('./auth').verifyToken;
 const express = require('express');
 const jwt_decode = require('jwt-decode');
 const router = express.Router();
+const fs = require('fs')
+const config = require('../config.json');
+
 
 router.post('/course', VerifyToken, async (req, res) => {
   try {
@@ -24,12 +27,17 @@ router.post('/course', VerifyToken, async (req, res) => {
     }
 
     // get user grades if any
-    let grades = await User.findOne({ "_id": req.body.userID }, 'coursesQuizes')
-    if (grades.coursesQuizes[0] != undefined && grades.coursesQuizes[0][req.body.id] != undefined) {
-      grades = grades.coursesQuizes[0][req.body.id]
-      let keys = Object.keys(grades)
-      for (let i = 0; i < keys.length; i++) {
-        course.modules[keys[i]]["grade"] = grades[keys[i]]
+    let data = await User.findOne({ "_id": req.body.userID }, 'coursesData')
+    for (let i = 0; i < course.modules.length; i++) {
+      if (data.coursesData[0] != undefined) {
+        if (data.coursesData[0][req.body.id] != undefined) {
+          if (data.coursesData[0][req.body.id][i] != undefined) {
+            if (data.coursesData[0][req.body.id][i].status != undefined)
+              course.modules[i]["completed"] = data.coursesData[0][req.body.id][i].status
+            if (data.coursesData[0][req.body.id][i].score != undefined)
+              course.modules[i]["grade"] = data.coursesData[0][req.body.id][i].score
+          }
+        }
       }
     }
 
@@ -43,7 +51,7 @@ router.post('/course', VerifyToken, async (req, res) => {
 
 })
 
-router.post('/course/update', async (req, res) => {
+router.post('/course/update', VerifyToken, async (req, res) => {
   try {
     const update = await Course.updateOne(
       { _id: req.body.courseID }, // query parameter
@@ -51,10 +59,12 @@ router.post('/course/update', async (req, res) => {
         $set: {
           name: req.body.name,
           description: req.body.description,
+          // urlImage: req.body.courseImage
         }
       })
 
-    res.json({ 'status': 'course added' });
+    // console.log('here')
+    res.json({ 'status': 'course updated' });
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
@@ -62,38 +72,20 @@ router.post('/course/update', async (req, res) => {
 
 })
 
-router.post('/course/enrollment', VerifyToken, async (req, res) => {
-
+router.post('/course/updateImage', VerifyToken, async (req, res) => {
   try {
+    const pathname = req.body.imageLink.split('/')
+    const imageName = pathname[pathname.length - 1]
 
-    // studentExists is a variable that will tell me if the course contains the user as an enrolled student
-    let studentExists = {}
-    studentExists = await Course.findOne({ "_id": req.body.courseID, "studentsEnrolled": req.body.userID }, '_id studentsEnrolled')
+    const update = await Course.updateOne(
+      { _id: req.body.courseID },
+      {
+        $set: {
+          urlImage: config.server_url + '/' + req.body.courseID + '/' + imageName
+        }
+      })
 
-    if (studentExists === null) {
-
-      const updateCourse = await Course.updateOne(
-        { _id: req.body.courseID },
-        {
-          $push: {
-            studentsEnrolled:
-              req.body.userID
-          },
-          $inc: { totalStudents: 1 }
-        });
-
-      const updateUser = await User.updateOne(
-        { _id: req.body.userID },
-        {
-          $push: {
-            enrolledClasses:
-              req.body.courseID
-          }
-
-        });
-
-      res.json({ 'status': 'student enrolled in course' });
-    }
+    res.json({ 'status': 'success' })
 
   } catch (e) {
     console.log(e);
@@ -101,6 +93,46 @@ router.post('/course/enrollment', VerifyToken, async (req, res) => {
   }
 
 })
+
+// router.post('/course/enrollment', VerifyToken, async (req, res) => {
+
+//   try {
+
+//     // studentExists is a variable that will tell me if the course contains the user as an enrolled student
+//     let studentExists = {}
+//     studentExists = await Course.findOne({ "_id": req.body.courseID, "studentsEnrolled": req.body.userID }, '_id studentsEnrolled')
+
+//     if (studentExists === null) {
+
+//       const updateCourse = await Course.updateOne(
+//         { _id: req.body.courseID },
+//         {
+//           $push: {
+//             studentsEnrolled:
+//               req.body.userID
+//           },
+//           $inc: { totalStudents: 1 }
+//         });
+
+//       const updateUser = await User.updateOne(
+//         { _id: req.body.userID },
+//         {
+//           $push: {
+//             enrolledClasses:
+//               req.body.courseID
+//           }
+
+//         });
+
+//       res.json({ 'status': 'student enrolled in course' });
+//     }
+
+//   } catch (e) {
+//     console.log(e);
+//     res.sendStatus(500);
+//   }
+
+// })
 
 router.post('/info', VerifyToken, async (req, res) => {
   try {
@@ -110,12 +142,12 @@ router.post('/info', VerifyToken, async (req, res) => {
       const query = req.body.search_query;
       courses = await Course.find({
         $or: [
-          { "category": { "$regex": query, $options: 'i' } },
+          { "categories": { "$regex": query, $options: 'i' } },
           { "name": { "$regex": query, $options: 'i' } },
         ]
-      }, '_id name description urlImage category', { limit: req.body.cardAmount }).skip(req.body.skip);
+      }, '_id name description urlImage categories', { limit: req.body.cardAmount }).skip(req.body.skip);
     } else {
-      courses = await Course.find({}, '_id name description urlImage category', { limit: req.body.cardAmount }).sort({ totalStudents: -1 }).skip(req.body.skip);
+      courses = await Course.find({}, '_id name description urlImage categories', { limit: req.body.cardAmount }).sort({ totalStudents: -1 }).skip(req.body.skip);
     }
 
     res.json({ "courses": courses });
@@ -124,6 +156,30 @@ router.post('/info', VerifyToken, async (req, res) => {
     res.sendStatus(500);
   }
 
+})
+
+router.post('/myCreatedCoursesInfo', VerifyToken, async (req, res) => {
+  try {
+
+    let courses = []
+    const user = await User.findOne({ _id: req.body.userID })
+    if (req.body.search_query != undefined) {
+      const query = req.body.search_query;
+      courses = await Course.find({
+        $and: [
+          { _id: user.createdCourses },
+          { $or: [{ "categories": { "$regex": query, $options: 'i' } }, { "name": { "$regex": query, $options: 'i' } }] },
+        ]
+      }), '_id name description urlImage categories'
+    }
+    else {
+      courses = await Course.find({ _id: user.createdCourses }, '_id name description urlImage categories')
+    }
+    res.json({ "courses": courses });
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 })
 
 router.post('/myCoursesInfo', VerifyToken, async (req, res) => {
@@ -138,12 +194,12 @@ router.post('/myCoursesInfo', VerifyToken, async (req, res) => {
       courses = await Course.find({
         $and: [
           { _id: user.enrolledClasses },
-          { $or: [{ "category": { "$regex": query, $options: 'i' } }, { "name": { "$regex": query, $options: 'i' } }] },
+          { $or: [{ "categories": { "$regex": query, $options: 'i' } }, { "name": { "$regex": query, $options: 'i' } }] },
         ]
-      }), '_id name description urlImage category'
+      }), '_id name description urlImage categories'
     }
     else {
-      courses = await Course.find({ _id: user.enrolledClasses }, '_id name description urlImage category')
+      courses = await Course.find({ _id: user.enrolledClasses }, '_id name description urlImage categories')
     }
 
     res.json({ "courses": courses });
@@ -161,13 +217,24 @@ router.post('/create', VerifyToken, async (req, res) => {
       name: req.body.name,
       description: req.body.description,
       urlImage: req.body.urlImage,
-      category: req.body.category,
+      categories: req.body.categories,
       author: req.body.userID
     })
 
     const savedCourse = await course.save();
 
-    console.log('added course ', savedCourse._id);
+    findCourse = await Course.findOne({ "name": req.body.name, "description": req.body.description }, '_id')
+    // console.log(findCourse._id)
+    const updateUser = await User.updateOne(
+      { _id: req.body.userID },
+      {
+        $push: {
+          createdCourses:
+            findCourse._id.toString()
+        }
+      });
+
+    // console.log('added course ', savedCourse._id);
 
     res.json(savedCourse);
   } catch (e) {
@@ -177,26 +244,75 @@ router.post('/create', VerifyToken, async (req, res) => {
 
 })
 
-router.post('/removeCourse', VerifyToken, async (req, res) => {
+router.post('/removeEnrollment', VerifyToken, async (req, res) => {
 
   try {
+
+    const find = await User.findOne({ _id: req.body.userID }, 'coursesData')
+
+    delete find.coursesData[0][req.body.courseID]
+  
     const update = await User.updateOne(
       { _id: req.body.userID },
-      { $pull: { enrolledClasses: req.body.courseID } }
+      {
+        $pull: { enrolledClasses: req.body.courseID },
+        $set: { coursesData: find.coursesData}
+      }
     )
 
     const updateCourse = await Course.updateOne(
       { _id: req.body.courseID },
       {
-        $pull: { studentsEnrolled: req.body.userID },
-        $inc: { totalStudents: -1 }
+        $pull: { studentsEnrolled: req.body.userID }
+        // $inc: { currStudents: -1 }
       })
+
+    res.json({'status': 'success'})
 
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
   }
 
+})
+
+// TODO: Need to move this to fileMulter once I figure out why it's not getting called when it sits in fileMulter
+router.post('/removeFile', VerifyToken, async (req, res) => {
+
+  const pathname = req.body.imageName.split('/')
+  const imageName = pathname[pathname.length - 1]
+  console.log(imageName)
+  const path = 'public/' + req.body.courseID + '/' + imageName
+
+  try {
+    fs.unlinkSync(path)
+    res.json({ 'status': 'module added' });
+
+  } catch (err) {
+    console.error(err)
+    res.sendStatus(500);
+  }
+
+})
+
+router.post('/deleteCreatedCourse', VerifyToken, async (req, res) => {
+
+  try {
+    const update = await User.updateOne(
+      { _id: req.body.userID },
+      { $pull: { createdCourses: req.body.courseID } }
+    )
+
+    const updateCourse = await Course.deleteOne({ _id: req.body.courseID })
+
+    fs.rmdirSync('public/' + req.body.courseID, { recursive: true });
+
+    res.sendStatus(400);
+
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 })
 
 router.post('/module/create', VerifyToken, async (req, res) => {
@@ -211,6 +327,49 @@ router.post('/module/create', VerifyToken, async (req, res) => {
               type: req.body.type,
               description: req.body.description,
               quiz: req.body.quiz,
+              gradeToPass: req.body.gradeToPass
+            }
+          }
+        });
+    } else if (req.body.type === "Video") {
+      const update = await Course.updateOne(
+        { _id: req.body.courseID }, // query parameter
+        {
+          $push: {
+            modules: {
+              title: req.body.title,
+              type: req.body.type,
+              description: req.body.description,
+              urlVideo: req.body.urlVideo,
+              videoDescription: req.body.videoDescription
+            }
+          }
+        });
+    } else if (req.body.type === "File") {
+      const update = await Course.updateOne(
+        { _id: req.body.courseID }, // query parameter
+        {
+          $push: {
+            modules: {
+              title: req.body.title,
+              type: req.body.type,
+              description: req.body.description,
+              urlFile: req.body.urlFile,
+              fileDescription: req.body.fileDescription
+            }
+          }
+        });
+    } else if (req.body.type === "PDF") {
+      const update = await Course.updateOne(
+        { _id: req.body.courseID }, // query parameter
+        {
+          $push: {
+            modules: {
+              title: req.body.title,
+              type: req.body.type,
+              description: req.body.description,
+              urlFile: req.body.urlFile,
+              fileDescription: req.body.fileDescription
             }
           }
         });
@@ -228,7 +387,7 @@ router.post('/module/create', VerifyToken, async (req, res) => {
         });
     }
 
-    res.json({ 'status': 'course added' });
+    res.json({ 'status': 'module added' });
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
@@ -237,28 +396,172 @@ router.post('/module/create', VerifyToken, async (req, res) => {
 
 router.post('/module/score', VerifyToken, async (req, res) => {
   try {
-    let courses = await User.findOne({ _id: req.body.userID }, 'coursesQuizes');
-    courses = courses.coursesQuizes[0];
+
+    let modules = await Course.findOne({ _id: req.body.courseID }, 'modules');
+    gradeToPass = modules.modules[req.body.moduleID].gradeToPass
+
+    let courses = await User.findOne({ _id: req.body.userID }, 'coursesData');
+
+    if (courses.coursesData[0] == undefined)
+      courses.coursesData.append({})
+
+    courses = courses.coursesData[0];
     if (courses === undefined)
       courses = {}
 
-    if (courses[req.body.courseID] !== undefined) { // exist course
-      courses[req.body.courseID][req.body.moduleID] = req.body.score
-    } else { // not course 
+    if (courses[req.body.courseID] === undefined)
       courses[req.body.courseID] = {}
-      courses[req.body.courseID][req.body.moduleID] = req.body.score
-    }
 
+    if (courses[req.body.courseID][req.body.moduleID] === undefined)
+      courses[req.body.courseID][req.body.moduleID] = {}
+
+    courses[req.body.courseID][req.body.moduleID]["score"] = req.body.score
+
+    if (gradeToPass <= req.body.score) {
+
+      if (courses[req.body.courseID][req.body.moduleID]["status"] != 1) {
+        if (modules.modules.length == req.body.moduleID + 1) {
+          const updateCourse = await Course.updateOne(
+            { _id: req.body.courseID },
+            {
+              $inc: { totalCompletedStudents: 1 }
+            });
+        }
+        courses[req.body.courseID][req.body.moduleID]["status"] = 1
+      }
+    }
 
     const update = await User.updateOne(
       { _id: req.body.userID }, // query parameter
       {
         $set: {
-          coursesQuizes: courses
+          coursesData: courses
         }
       });
 
-    res.json({ 'status': 'course added' });
+
+    res.json({ 'status': 'grade saved' });
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+})
+
+router.post('/module/update', VerifyToken, async (req, res) => {
+  try {
+    if (req.body.type === "Quiz") {
+      const update = await Course.updateOne(
+        { _id: req.body.courseID }, // query parameter
+        {
+          $set: {
+            [`modules.${req.body.moduleID}`]: {
+              title: req.body.title,
+              type: req.body.type,
+              description: req.body.description,
+              quiz: req.body.quiz,
+            }
+          }
+        });
+    } else {
+      const update = await Course.updateOne(
+        { _id: req.body.courseID }, // query parameter
+        {
+          $set: {
+            [`modules.${req.body.moduleID}`]: {
+              title: req.body.title,
+              type: req.body.type,
+              description: req.body.description,
+            }
+          },
+        });
+    }
+
+    res.json({ 'status': 'module updated' });
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+})
+
+router.post('/module/delete', VerifyToken, async (req, res) => {
+
+  try {
+    const update = await Course.updateOne(
+      { _id: req.body.courseID },
+      { $pull: { modules: { title: req.body.title, description: req.body.description } } }
+    )
+
+    res.json({ 'status': 'success' })
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+})
+
+router.post('/module/completed', VerifyToken, async (req, res) => {
+  try {
+    let courses = await User.findOne({ _id: req.body.userID }, 'coursesData');
+
+    courses = courses.coursesData[0];
+    if (courses === undefined)
+      courses = {}
+
+    if (courses[req.body.courseID] !== undefined) { // exist course
+      courses[req.body.courseID][req.body.moduleID] = { status: 1 }
+    } else { // not course 
+      courses[req.body.courseID] = {}
+      courses[req.body.courseID][req.body.moduleID] = { status: 1 }
+    }
+
+    const update = await User.updateOne(
+      { _id: req.body.userID }, // query parameter
+      {
+        $set: {
+          coursesData: courses
+        }
+      });
+
+    // If the user completed the first module, do the checks for enrollment. Otherwise, proceed as usual
+    if (req.body.moduleID == 0) {
+      let studentExists = {}
+      studentExists = await Course.findOne({ "_id": req.body.courseID, "studentsEnrolled": req.body.userID }, '_id studentsEnrolled')
+
+      if (studentExists === null) {
+
+        const updateCourse = await Course.updateOne(
+          { _id: req.body.courseID },
+          {
+            $push: {
+              studentsEnrolled:
+                req.body.userID
+            },
+            $inc: { totalStudents: 1 }
+          });
+
+        const updateUser = await User.updateOne(
+          { _id: req.body.userID },
+          {
+            $push: {
+              enrolledClasses:
+                req.body.courseID
+            }
+
+          });
+
+        // res.json({ 'status': 'student enrolled in course' });
+      }
+    }
+
+    let modules = await Course.findOne({ _id: req.body.courseID }, 'modules');
+    if (modules.modules.length == req.body.moduleID + 1) {
+      const updateCourse = await Course.updateOne(
+        { _id: req.body.courseID },
+        {
+          $inc: { totalCompletedStudents: 1 }
+        });
+    }
+
+    res.json({ 'status': 'saved' });
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
