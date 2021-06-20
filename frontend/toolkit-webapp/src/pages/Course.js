7f6153fcad6e2bac943768f3259d208b5f694c1d
Michael-Dews <79720787+Michael-Dews@users.noreply.github.com>
2021-06-20 14:53:04 -0400

    testing

--- frontend/toolkit-webapp/src/pages/Course.js
+++ frontend/toolkit-webapp/src/pages/Course.js
@@ -1,37 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import config from '../config.json'
 import TopNavBar from '../components/TopNavBar'
-import { Divider, makeStyles, Grid, Typography, TextField, Button, Container } from '@material-ui/core'
+import { IconButton, Link, FormControlLabel, Divider, makeStyles, Grid, Typography, TextField, Button, Container } from '@material-ui/core'
 import VideoModule from '../components/VideoModule'
 import PdfModule from '../components/PdfModule'
 import QuizModule from '../components/QuizModule'
+import FileModule from '../components/FileModule'
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import CourseInfoEditButton from '../components/CourseInfoEditButton';
 import ModuleInfoEditButton from '../components/ModuleInfoEditButton';
-import courseStyles from '../styles/courseStyle'
-import jwt_decode from "jwt-decode";
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import courseStyles from '../styles/courseStyle';
 
 
 const Course = (props) => {
   const [course, setCourse] = useState({})
   const [modules, setModules] = useState([])
+  const [oldCourseImage, setOldCourseImage] = useState('')
+  const [currCourseImage, setCurrCourseImage] = useState('')
   const [courseTitle, setCourseTitle] = useState('')
   const [courseDescription, setCourseDescription] = useState('')
   const [editCourseInfo, setEditCourseInfo] = useState(false)
   const [courseID, setCourseID] = useState('')
 
+  let validImageTypes = ["png", "PNG", "jpeg", "jpg"]
+
   const classes = courseStyles()
 
   const onEditCourseTitle = (e) => {
     setEditCourseInfo(true);
-  };
-
-  const onEditModule = (e) => {
-    //alert("you have pressed the edit button the course ID is = " + courseID)
-    props.history.push(`/ModuleManager/${courseID}`);
   };
 
   // function that will run when page is loaded
@@ -54,9 +55,12 @@ const Course = (props) => {
     })
 
     const data = await res.json()
+
     if (data.message === undefined) {
       setCourse(data.course);
       setCourseID(id);
+      setOldCourseImage(data.course.urlImage);
+      setCurrCourseImage(data.course.urlImage);
       setCourseTitle(data.course.name);
       setCourseDescription(data.course.description);
       setModules(data.course.modules);
@@ -70,23 +74,110 @@ const Course = (props) => {
   }
 
   const onEditSubmit = async (e) => {
-
+    // e.preventDefault()
     setEditCourseInfo(false);
-    console.log("this is the title:" + course.name)
-    console.log(courseDescription)
-    console.log(config.server_url + config.paths.updateCourseInfo)
-    if (courseTitle == '') { setCourseTitle(course.name) }
+    const token = localStorage.getItem("token");
+
     const res = await fetch(config.server_url + config.paths.updateCourseInfo, {
       method: 'POST',
       headers: {
         'Content-type': 'application/json'
       },
       body: JSON.stringify({
+        'token': token,
         'courseID': courseID,
         "name": courseTitle,
         "description": courseDescription,
       })
     })
+
+    const data = await res.json()
+
+    // No new image assigned to course so only refresh to show other updates
+    if (currCourseImage.name === undefined)
+      window.location.reload();
+
+
+    // We have a new image being passed in so delete old file
+    if ((oldCourseImage !== null) && (oldCourseImage.name !== currCourseImage.name)) {
+
+      const res = await fetch(config.server_url + config.paths.removeFile, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          'token': token,
+          'courseID': courseID,
+          'imageName': oldCourseImage
+        })
+      })
+    }
+
+
+    const imageData = new FormData();
+    imageData.append('file', currCourseImage)
+
+
+    // Checking to see if the file inputted is not an actual image
+    const imageTypePath = currCourseImage.name.split('.')
+    const imageType = imageTypePath[imageTypePath.length - 1]
+    const validInput = validImageTypes.includes(imageType);
+
+    // If it isn't, return and allow user to input valid image
+    if (!validInput) {
+      alert('Invalid file type. Please upload an image with the extension .jpg or .png')
+      return
+    }
+
+    if (currCourseImage.name !== oldCourseImage.name) {
+
+      if (data.message === undefined) {
+        const res = await fetch(config.server_url + config.paths.fileUpload + "?token=" + token + "&courseID=" + courseID + "&imageName=" + currCourseImage.name, {
+          method: 'POST',
+          body: imageData
+        })
+        const data2 = await res.json()
+
+      }
+      else { // this is to check if there are errors not being addressed already
+        console.log(data)
+      }
+    }
+    else {
+      const res = await fetch(config.server_url + config.paths.updateCourseImage, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          'token': token,
+          'courseID': courseID,
+          "imageLink": currCourseImage,
+        })
+      })
+    }
+
+    window.location.reload();
+  }
+
+  const deleteModule = async (module) => {
+    const token = localStorage.getItem("token");
+    const res = await fetch(config.server_url + config.paths.deleteModule, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        'token': token,
+        'courseID': course._id,
+        // 'moduleID': indexOf(module),
+        'title': module.title,
+        'description': module.description,
+      })
+    })
+
+    const data = await res.json()
 
     window.location.reload();
   }
@@ -105,10 +196,53 @@ const Course = (props) => {
         body: JSON.stringify({
           "courseID": course._id,
           "token": token
-          // "userID": decoded.id
         })
       })
     }
+
+  }
+
+  const isDisabled = (index) => {
+    if (index >= 3) {
+      if (modules[index - 1].completed === 1) {
+        return false
+      }
+      return true
+    }
+    return false
+  }
+
+  const handleComplete = async (index) => {
+    // send the completed news to db
+    const token = localStorage.getItem("token");
+    const res = await fetch(config.server_url + config.paths.completedModule, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        "courseID": courseID,
+        "token": token,
+        "moduleID": index
+      })
+    })
+
+    const data = await res.json()
+    if (data.message === undefined) {
+      let temp = modules;
+      temp[index]["completed"] = 1
+      setModules(temp)
+      window.location.reload();
+
+    } else if (data.message === "wrong token") {
+      localStorage.removeItem('token');
+      props.history.push('login');
+      // probably alert the user
+
+    } else { // this is to check if there are errors not being addressed already
+      console.log(data)
+    }
+
 
   }
 
@@ -121,8 +255,8 @@ const Course = (props) => {
           <div maxWidth>
             <Grid item alignItems="center" xs={12}>
               <Grid container className={classes.topItem}>
-                <Grid item xs={3} sm={2} lg={1}>
-                  <img src={course.urlImage} className={classes.courseImageStyle} />
+                <Grid item xs={3} sm={2} lg={1} >
+                  <img src={course.urlImage} className={classes.currCourseImageStyle} />
                 </Grid>
                 <Grid item xs={8} sm={9} lg={9}>
                   <h1
@@ -146,13 +280,14 @@ const Course = (props) => {
             <Grid item xs={12} >
               <Grid container className={classes.topItem}>
                 <Grid item xs={3} sm={2} lg={1}>
-                  <img src={course.urlImage} className={classes.courseImageStyle} />
+                  <img src={course.urlImage} className={classes.currCourseImageStyle} />
                 </Grid>
                 <Grid item xs={9} sm={10} lg={11} align={"center"}>
                   <TextField
                     color='primary'
                     size='medium'
-                    variant="filled"
+                    variant="outlined"
+                    inputProps={{ style: { textAlign: 'center' } }}
                     label='Title'
                     type="text"
                     defaultValue={course.name}
@@ -162,62 +297,156 @@ const Course = (props) => {
                   //style={{ backgroundColor: "rgba(255,255,255,0.8)" }}
                   />
                 </Grid>
+                <input type="file" name="picture" accept="image/*" className={classes.currCourseImageStyle} onChange={e => setCurrCourseImage(e.target.files[0])} />
               </Grid>
             </Grid>
-            <Grid item xs={12} >
+            <Grid item xs={12} lg={6}>
               <TextField
                 color='primary'
                 size='medium'
-                variant="filled"
+                variant='filled'
                 label='Description'
                 type="text"
                 defaultValue={course.description}
-                //value={course.description}
                 onChange={e => setCourseDescription(e.target.value)}
                 margin="normal"
                 required={true}
                 fullWidth
                 multiline
-                rows={10}
-                rowsMax={15}
-              //style={{ backgroundColor: "rgba(255,255,255,0.8)" }}
+                rows={4}
+                rowsMax={10}
               />
             </Grid>
-            <Button onClick={onEditSubmit}>Submit</Button>
+            <Button variant="contained" onClick={onEditSubmit}>Submit</Button>
           </div>
         }
+
+        <br></br>
         <Grid item xs={12}>
           <Divider className={classes.divider} />
         </Grid>
-        <ModuleInfoEditButton edit={onEditModule} id={courseID} hideComponent={false} />
+        <Grid item xs={12} lg={3}>
+          <Link href={`/newModule/${courseID}`} underline={'none'}>
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              startIcon={<AddIcon />}
+            >
+              Add Module
+            </Button>
+          </Link>
+        </Grid>
         <Grid item xs={12} className={classes.accordion}>
-
           {/* modules starts here */}
           {modules.map((module) => (
-            <Accordion key={modules.indexOf(module)} onClick={e => enroll(module)} >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography className={classes.heading}>Module {modules.indexOf(module) + 1}: {module.title}</Typography>
-              </AccordionSummary>
-              <AccordionDetails className={classes.accordionDetails}>
-                <Typography >
-                  {/* Type: {module.type} */}
-                  {module.type == "Quiz" && <Typography >Grade: {module.grade}/{module.quiz.length}</Typography>}
-                  <br />
-                  {module.description}
-                  <br />
-                  <br />
-                  <div >
-                    {module.type === "Video" && <VideoModule fileUrl={module.fileUrl} />}
-                    {module.type === "Pdf" && <PdfModule fileUrl={module.fileUrl} />}
-                    {module.type === "Quiz" && <QuizModule quiz={module.quiz} moduleIndex={modules.indexOf(module)} courseID={courseID} />}
-                  </div>
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
+            <div>
+              {isDisabled(modules.indexOf(module)) ?
+                <Accordion key={modules.indexOf(module)} disabled >
+
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <FormControlLabel
+                      aria-label="Acknowledge"
+                      onClick={(event) => event.stopPropagation()}
+                      onFocus={(event) => event.stopPropagation()}
+                      control={<ModuleInfoEditButton moduleIndex={modules.indexOf(module)} courseID={courseID} module={module} hideComponent={false} />}
+                    />
+                    <FormControlLabel
+                      aria-label="Acknowledge"
+                      onClick={(event) => event.stopPropagation()}
+                      onFocus={(event) => event.stopPropagation()}
+                      control={
+                        <IconButton
+                          type='submit'
+                          className={classes.deleteButton}
+                          variant="contained"
+                          color="secondary"
+                          onClick={() => window.confirm('Are you sure you wish to delete this module: ' + (modules.indexOf(module) + 1) + '?') && deleteModule(module)}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      }
+                    />
+                    <Typography className={classes.heading}>Module {modules.indexOf(module) + 1}: {module.title}</Typography>
+                  </AccordionSummary>
+                </Accordion>
+                :
+                <Accordion key={modules.indexOf(module)} >
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <FormControlLabel
+                      aria-label="Acknowledge"
+                      onClick={(event) => event.stopPropagation()}
+                      onFocus={(event) => event.stopPropagation()}
+                      control={<ModuleInfoEditButton moduleIndex={modules.indexOf(module)} courseID={courseID} module={module} hideComponent={false} />}
+                    />
+                    <FormControlLabel
+                      aria-label="Acknowledge"
+                      onClick={(event) => event.stopPropagation()}
+                      onFocus={(event) => event.stopPropagation()}
+                      control={
+                        <IconButton
+                          type='submit'
+                          className={classes.deleteButton}
+                          variant="contained"
+                          color="secondary"
+                          onClick={() => window.confirm('Are you sure you wish to delete this module: ' + (modules.indexOf(module) + 1) + '?') && deleteModule(module)}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      }
+                    />
+                    <Typography className={classes.heading}>Module {modules.indexOf(module) + 1}: {module.title}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails className={classes.accordionDetails}>
+                    <div className={classes.accordionDiv}>
+                      <Typography >
+
+                        {/* Type: {module.type} */}
+                        {module.type == "Quiz" &&
+                          <div>
+                            <Typography >Grade: {module.grade}/{module.quiz.length}</Typography>
+                            <Typography>Grade needed to pass: {module.gradeToPass}/{module.quiz.length}</Typography>
+                          </div>
+                        }
+                        <br />
+                        {module.description}
+                      </Typography>
+                      <br />
+                      <br />
+                      <div className={classes.fileDiv}>
+                        {module.type === "Video" && <VideoModule videoUrl={module.urlVideo} />}
+                        {module.type === "PDF" && <PdfModule fileUrl={module.urlFile} />}
+                        {module.type === "File" && <FileModule fileUrl={module.urlFile} />}
+                        {module.type === "Quiz" && <QuizModule quiz={module.quiz} moduleIndex={modules.indexOf(module)} courseID={courseID} />}
+                      </div>
+                      <br />
+                      {module.type !== "Quiz" &&
+                        <div className={classes.completeDiv}>
+                          {/* {module.completed === 1 ?
+                            <Button disabled>Complete!</Button> */}
+                          {/* : */}
+                          <Button
+                            variant="contained"
+                            onClick={() => handleComplete(modules.indexOf(module))}
+                            disabled={module.completed === 1}
+                          >
+                            Complete!
+                          </Button>
+                          {/* } */}
+                        </div>
+                      }
+                    </div>
+                  </AccordionDetails>
+                </Accordion>}
+            </div>
           ))}
         </Grid>
       </Grid>
